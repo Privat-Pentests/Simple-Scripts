f0d1b90010519ec40a5d9ff91339f7f056d669c4
Dominik <admin@dominik48n.de>
2021-05-15 11:57:25 +0200

    Implement a time stamp

--- check-subdomains/__main__.py
+++ check-subdomains/__main__.py
@@ -1,12 +1,15 @@
 import requests
 import sys
 import os
+import time
 
 domain = sys.argv[1]
 file = open("{}//subdomains.txt".format(os.path.dirname(__file__)))
 subdomains = []
 
 if __name__ == '__main__':
+    startTime = round(time.time() * 1000)
+    found = []
 
     if len(sys.argv) > 1:
         for i in file.readlines():
@@ -22,7 +25,10 @@ if __name__ == '__main__':
             except:
                 pass
             else:
+                found.append(subdomain)
                 print('Found: {}'.format(subdomain, domain))
+
+        print("Founded {} Subdomains of {} in {}ms.".format(len(found), domain, round(time.time() * 1000) - startTime))
     else:
         print("Please enter a domain.")
         pass
